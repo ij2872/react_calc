e805fe8c8c5e9a47cd27e0fff737e36addf2e1f1
ij2872 <ijaimes2872@gmail.com>
2017-08-21 20:42:40 -0500

    init commit

---
+++ src/components/calc/Calculator.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './calc.css'
+class componentName extends Component {
+    render() {
+        return (
+            <div className="calculator">
+                <div className="results">215231532233312</div>  
+                <div className="btn" id="ac">AC</div>
+                <div className="btn" id="pn">+/-</div>
+                <div className="btn" id="percent">%</div>
+                <div className="btn" id="divide">/</div>
+ 
+                <div className="btn" id="seven">7</div>
+                <div className="btn" id="eight">8</div>
+                <div className="btn" id="nine">9</div>
+                <div className="btn" id="times">X</div>
+
+                <div className="btn" id="four">4</div>
+                <div className="btn" id="five">5</div>
+                <div className="btn" id="six">6</div>
+                <div className="btn" id="subtract">-</div>
+
+                <div className="btn" id="one">1</div>
+                <div className="btn" id="two">2</div>
+                <div className="btn" id="three">3</div>
+                <div className="btn" id="add">+</div>
+
+                <div className="btn" id="zero">7</div>
+                <div className="btn" id="decimal">.</div>
+                <div className="btn" id="nine">9</div>
+
+                
+            </div>
+        );
+    }
+}
+
+export default componentName;
